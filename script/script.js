c9a981c127f00f6829ff111cffa5d3072b8b0c4b
NaelYo <naeltampan@gmail.com>
2024-04-05 11:54:04 +0700

    update dark-theme

--- script/script.js
+++ script/script.js
@@ -1,4 +1,17 @@
+// Sticky Navbar
 window.addEventListener('scroll', function() {
     var header = document.querySelector('nav');
     header.classList.toggle('sticky', window.scrollY > 0)
 })
+
+// Dark Theme
+const icon = document.getElementById('icon');
+
+icon.onclick = function() {
+    document.body.classList.toggle('dark-theme')
+    if (document.body.classList.contains('dark-theme')) {
+        icon.src = 'img/dark theme icon/sun.png'
+    } else {
+        icon.src = 'img/dark theme icon/moon.png'
+    }
+}
