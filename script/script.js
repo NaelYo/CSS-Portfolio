b22c3ec5230802ba3a473190df3659782c8aa90a
NaelYo <naeltampan@gmail.com>
2024-04-05 20:49:32 +0700

    last update theme

--- script/script.js
+++ script/script.js
@@ -10,8 +10,8 @@ const icon = document.getElementById('icon');
 icon.onclick = function() {
     document.body.classList.toggle('dark-theme')
     if (document.body.classList.contains('dark-theme')) {
-        icon.src = 'img/theme/moon-icon.png'
+        icon.src = 'img/theme/sun.png'
     } else {
-        icon.src = 'img/theme/sun-icon.png'
+        icon.src = 'img/theme/moon.png'
     }
 }
